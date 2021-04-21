3df9b9b3cabf71517b6268b8376e13373b12dddb
Yuri Nedilsky <ur1k9801@gmail.com>
2021-04-21 23:16:21 +0300

    22. parameters transfer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  posts: Post[] = [
+    {title: 'This is first post', text: 'This is first post text lorem', id: 1},
+    {title: 'Definitely cool post heading', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, quia!', id: 2}
+    ];
 
 }
 
