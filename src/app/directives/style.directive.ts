c5a5ca5535c465beca6f94dd8d42abed57ce08b5
Yuri Nedilsky <ur1k9801@gmail.com>
2021-04-28 00:19:57 +0300

    33. Parameters transfer to directive

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,12 +1,29 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
 
+  @Input('appStyle') color = 'blue';
+  @Input() dStyles: {border?: string, fontWeight?: string, borderRadius?: string};
+
   constructor(private el: ElementRef, private r: Renderer2) {
-    this.r.setStyle(this.el.nativeElement, 'color', 'rgb(241 145 145)');
   }
 
+  @HostListener('click', ['$event.target']) onClick(event: Event): void {
+    console.log(event);
+  }
+  @HostListener('mouseenter') onEnter(): void {
+    this.r.setStyle(this.el.nativeElement, 'color', this.color);
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight);
+    this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border);
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius);
+  }
+  @HostListener('mouseleave') onLeave(): void {
+    this.r.setStyle(this.el.nativeElement, 'color', null);
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
+    this.r.setStyle(this.el.nativeElement, 'border', null);
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', null);
+  }
 }
