edfc59824556d627d989625bab6fc55492aae46c
Yuri Nedilsky <ur1k9801@gmail.com>
2021-04-27 23:51:00 +0300

    31. Directives: creation, access to element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,14 @@
 import {Component, OnInit, Output} from '@angular/core';
 
-export interface Post {
-  title: string;
-  text: string;
-  id?: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
-
 export class AppComponent implements OnInit {
-
-  posts: Post[] = [
-    {title: 'Definitely cool post heading', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, quia!', id: 0}
-    ];
-
-  postsCount: number = this.posts.length;
-
   ngOnInit() {
 
-  }
-
-  updatePosts(post: Post) {
-     this.posts.unshift(post);
-     this.postsCount = this.posts.length;
-  }
-
-  removePost(id: number) {
-    this.posts = this.posts.filter(p => p.id !== id)
   }
 }
 
---
+++ src/app/directives/style.directive.ts
@@ -0,0 +1,12 @@
+import {Directive, ElementRef, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appStyle]'
+})
+export class StyleDirective {
+
+  constructor(private el: ElementRef, private r: Renderer2) {
+    this.r.setStyle(this.el.nativeElement, 'color', 'rgb(241 145 145)');
+  }
+
+}
