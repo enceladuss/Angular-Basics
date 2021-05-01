e43c503a6ecf67743808ec39dee5ea1fd53db65f
Yuri Nedilsky <ur1k9801@gmail.com>
2021-05-01 13:44:21 +0300

    45. Async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import {Component, OnInit, Output} from '@angular/core';
+import {Observable} from 'rxjs';
 
-export interface Post {
-  title: string;
-  text: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -13,15 +10,11 @@ export interface Post {
 
 export class AppComponent  {
 
-  search = '';
-
-  searchInText = true;
-
-  posts: Post[] = [
-    {title: 'Beer', text: 'Some text about beeer, ...which production may use wheat'},
-    {title: 'Bread', text: 'Some text about bread ...most made out of wheat'},
-    {title: 'Javscript', text: 'Some text about javascript, ...by deafult has no typization'},
-  ];
+  date: Observable<Date> = new Observable(obs => {
+    setInterval(() => {
+      obs.next(new Date());
+    }, 1000);
+  });
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import {MultByPipe} from './pipes/mult-by.pipe';
-import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MultByPipe,
-    FilterPipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
