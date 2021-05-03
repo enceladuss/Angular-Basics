6f69c71af247f534c5a466fd3c0e90a7cefd1673
Yuri Nedilsky <ur1k9801@gmail.com>
2021-05-03 23:50:12 +0300

    49. Local and global services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import {AppCounterService} from './services/app-counter.service';
+import { CounterComponent } from './counter/counter.component';
+import {LocalCounterService} from './services/local-counter.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [AppCounterService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
