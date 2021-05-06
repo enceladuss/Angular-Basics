fde74f083e9a72ba90f1eb9e600f52ea274c1656
Yuri Nedilsky <ur1k9801@gmail.com>
2021-05-06 23:38:24 +0300

    65. Getting data and parsing to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import {Component, OnInit} from '@angular/core'
 import {HttpClient} from '@angular/common/http';
 
+export interface Todo {
+  completed: boolean
+  title: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +14,16 @@ import {HttpClient} from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
 
+  todos: Todo[] = []
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe(todos => {
+        console.log(todos)
+        this.todos = todos
+    })
   }
 }
 
