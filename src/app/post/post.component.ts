b309a1e73ebc97ea28f6b419ecdbc4dffb27eb0c
Yuri Nedilsky <ur1k9801@gmail.com>
2021-05-12 20:10:54 +0300

    84. Working with params

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,34 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Post, PostsService} from '../posts.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
 
+  post: Post;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private postService: PostsService
+  ) {
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const zlp = this.postService.getById(+params.id);
+      if (zlp) {
+        this.post = zlp;
+      }
+
+    });
+  }
+
+  loadPost() {
+    this.router.navigate(['/posts', 44])
+  }
 }
